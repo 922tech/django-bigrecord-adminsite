452c9d0b2ce20ff8066fe1a08d03ff4e16c5d180
922tech <922tech@gmail.com>
2023-12-29 13:34:35 +0330

    Refactoring django's pagination

--- books/admin.py
+++ books/admin.py
@@ -1,17 +1,13 @@
+from typing import Sequence
 from django.contrib import admin
 from django.contrib.admin.views.main import ChangeList
 from django.utils.functional import cached_property
 from django.core.paginator import Paginator
 from django.db import connection
-from .models import Book
-
+from django.core.paginator import InvalidPage
+from django.contrib.admin.options import IncorrectLookupParameters
+from .models import Book, Author
 from .utils import get_field_verbose_names, get_field_names
-
-
-class NonPaginator(Paginator):
-    @cached_property
-    def count(self):
-        return 10
 
 
 class SearchOnlyChangeList(ChangeList):
@@ -22,14 +18,29 @@ class SearchOnlyChangeList(ChangeList):
     """
 
     def __init__(self, *args, **kwargs):
-        self.search_result_count = None
+        self.search_result_count = 0
         super().__init__(*args, **kwargs)
-        self.search_fields = [self.all_fields[0]]
+        self.lookup_field = None
+
+    @property
+    def related_fields(self):
+        return [
+            i.split('__') for i in self.search_fields if '__' in i
+        ]
+
+    @staticmethod
+    def get_join_clause(seq: Sequence):
+        """
+        param seq: sequence of related fields to join
+        e.g. 
+        >>> get_join_clause([''])
+        """
+        return ' '.join('JOIN ')
 
     @property
     def all_fields(self):
         """
-        Returns all field names no a model in a list
+        Returns all field names on a model in a list
         """
         return get_field_names(self.model)
 
@@ -39,7 +50,7 @@ class SearchOnlyChangeList(ChangeList):
 
     @property
     def fields_enumeration(self):
-        return enumerate(self.all_fields_verbose_names)
+        return enumerate(self.search_fields)
 
     def set_search_fields(self, search_field_index=0):
         self.search_fields = [self.all_fields[search_field_index]]
@@ -74,13 +85,17 @@ class SearchOnlyChangeList(ChangeList):
                 q[i] += delim + ' '
         return ''.join(q)
 
+    def get_search_clause(self, field):
+        table = self.opts.db_table
+        return f"LOWER({table}.{field}::text) LIKE LOWER(%s::text) "
+
     def get_ordering_kwargs(self) -> str:
         """
         returns an `ORDER BY ...` SQL clause.
         Indirectly uses the `o` parameter of changelist request.GET
         (using `get_ordering_field_columns` method).
 
-        sample output for a model that has fields:id, author, name:
+        sample output for a model that has fields: id, author, name:
         >>> "ORDER BY id asc, author asc, title asc"
         """
 
@@ -91,44 +106,43 @@ class SearchOnlyChangeList(ChangeList):
 
         return kwargs_string
 
-    def get_sql(self, order_code: str = '', page_number=1) -> str:
+    def get_sql(self, order_code: str = '', page_number: int = 1) -> str:
         """
         param `order_code`: a django coding for changelist ordering
-        returns a complete sql query
+        returns a complete sql query with limit & offset
         """
+        root_query = str(self.root_queryset.query)
+        from_clause_index = root_query.capitalize().find('FROM')
+        select_clause = root_query[:from_clause_index]
 
-        searchparams = self.get_sql_searchparams()
+        searchparams = self.get_search_clause(self.lookup_field)
         if order_code:
             ordering_params = self.get_ordering_kwargs()
         else:
-            ordering_params = f'ORDER BY {self.model_admin.default_sorting_key} asc'
+            ordering_params = f"""
+            ORDER BY {self.model_admin.default_sorting_key} 
+            {self.model_admin.default_sorting_order}"""
 
-        sql = str(self.root_queryset.query) + f' {self.opts.db_table}' + \
-            ' WHERE ' + searchparams + ordering_params \
+        sql = root_query + ' WHERE ' + searchparams + ordering_params \
             + f' LIMIT {self.list_per_page}' + \
-              f' OFFSET { self.list_per_page * (page_number - 1) }'
-
+              f' OFFSET {self.list_per_page * (page_number - 1)}'
         return sql
 
-    def get_search_queryset(self, sql_string, searched_data):
-        queryset = self.model.objects.raw(
-            sql_string, [f'%{searched_data}%'] * len(self.search_fields)
-        )
+    def get_search_queryset(self, sql_string: str, searched_data: str):
+        queryset = self.model.objects.raw(sql_string, [f'%{searched_data}%'])
         return queryset
 
     def pagination_required(self, request):
         """deactivating django's default pagination"""
         return
 
-    def count_search_result(self, searched_data):
-        searchparams = self.get_sql_searchparams()
+    def count_search_result(self, searched_data: str) -> int:
+        searchparams = self.get_search_clause(self.lookup_field)
         sql = f"SELECT COUNT(*) FROM {self.opts.db_table} WHERE {searchparams}"
         cursor = connection.cursor()
-        cursor.execute(
-            sql, [f'%{searched_data}%'] * len(self.search_fields))
+        cursor.execute(sql, [f'%{searched_data}%'])
         self.search_result_count = cursor.fetchone()[0]
         return self.search_result_count
-        #  TODO: put the `search_param` and persistant data in the request.session
 
     def get_queryset(self, request):
         """
@@ -140,16 +154,15 @@ class SearchOnlyChangeList(ChangeList):
         if not q:
             return self.root_queryset.none()
 
-        if 'mf' in request_data:  # mf: model_field
-            # model field to lookup
+        if 'mf' in request_data:  # mf: model_field to look up for
             search_field_index = int(request_data['mf'])
-            self.set_search_fields(search_field_index)
+            self.lookup_field = self.search_fields[search_field_index]
 
         if q:
-            print(request.session.get('search_param'), 'search_param\n\n')
             if request.session.get('search_param') != q + request_data.get('mf'):
                 request.session['search_param'] = q + request_data.get('mf')
-                request.session['count'] = self.count_search_result(q)
+                self.model_admin.paginator.count = request.session['count'] = self.count_search_result(
+                    q)
             else:
                 self.search_result_count = request.session['count']
 
@@ -159,20 +172,59 @@ class SearchOnlyChangeList(ChangeList):
                 page_number = int(request_data['p'])
             else:
                 page_number = 1
-            sql_string = self.get_sql(order_code, page_number)
+
+            if '__' in self.lookup_field:
+                sql_string = ''
+            else:
+                sql_string = self.get_sql(order_code, page_number)
+
             queryset = self.get_search_queryset(sql_string, q)
             return queryset
 
+    def get_results(self, request):
+        paginator = self.model_admin.get_paginator(
+            request, self.queryset, self.list_per_page
+        )
+        # Get the number of objects, with admin filters applied.
+        result_count = self.search_result_count
+        # Get the total number of objects, with no admin filters applied.
+        if self.model_admin.show_full_result_count:
+            full_result_count = self.root_queryset.count()
+        else:
+            full_result_count = None
+        can_show_all = result_count <= self.list_max_show_all
+        multi_page = result_count > self.list_per_page
+
+        try:
+            result_list = self.queryset._clone()
+        except InvalidPage:
+            raise IncorrectLookupParameters
+
+        self.result_count = result_count
+        self.show_full_result_count = self.model_admin.show_full_result_count
+        # Admin actions are shown if there is at least one entry
+        # or if entries are not counted because show_full_result_count is disabled
+        self.show_admin_actions = not self.show_full_result_count or bool(
+            full_result_count
+        )
+        self.full_result_count = full_result_count
+        self.result_list = result_list
+        self.can_show_all = can_show_all
+        self.multi_page = multi_page
+        self.paginator = paginator
+
 
 class OptimizedAdminSearchMixin:
+    """
+    Wraps the whole optimized django admin site logic.
+    To use, inherit this class in your admin class along with django's ModelAdmin.
+    """
     default_sorting_key = 'id'
+    search_fields = ['id']
+    default_sorting_order = 'ASC'  # or 'DESC'
     list_per_page = 15
     change_list_template = 'admin/bigrecord_change_list.html'
     show_full_result_count = False
-    _paginator_cls = NonPaginator
-    _paginator_cls.count = list_per_page
-    paginator = _paginator_cls
-    # the value of list_per_page should be equal to the value of NonPaginator.count
 
     def get_changelist(self, request, **kwargs):
         return SearchOnlyChangeList
@@ -180,5 +232,11 @@ class OptimizedAdminSearchMixin:
 
 @admin.register(Book)
 class MyAdmin(OptimizedAdminSearchMixin, admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ['id', 'title', 'price']
     default_sorting_key = 'title'
+    search_fields = ['id', 'title', 'price', 'author__name']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
--- books/models.py
+++ books/models.py
@@ -1,17 +1,21 @@
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name='Book Title')
     publication_date = models.DateField(verbose_name='Published On')
     price = models.FloatField(verbose_name='Price')
-    serial_number = models.CharField(max_length=36, null=True, blank=True, verbose_name='Serial Number')
+    serial_number = models.CharField(
+        max_length=36, null=True, blank=True, verbose_name='Serial Number'
+    )
     description = models.TextField(verbose_name='Description')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
-    # class Meta:
-    #     db_table = 'book'
-
